04532a592f3848c7299504ded35a4389b24e4d72
MclPio <113801014+MclPio@users.noreply.github.com>
2023-04-03 21:06:40 -0400

    add functions for Board class

--- main.rb
+++ main.rb
@@ -5,15 +5,23 @@
 class Board
   attr_accessor :adj_list
 
-  def initialize()
+  def initialize
     @adj_list = [[], [], [], [], [], [], [], []]
+  end
+
+  def add_piece(coordinates)
+    adj_list[coordinates[0]] << coordinates[1]
+  end
+
+  def remove_piece(coordinates)
+    adj_list[coordinates[0]].pop(coordinates[1])
   end
 end
 
 class Knight
   attr_accessor :current_coordinate
 
-  def initialize(current_coordinate)
+  def initialize(current_coordinate = [2, 1])
     @current_coordinate = current_coordinate
   end
 
@@ -24,8 +32,13 @@ class Knight
 end
 
 board = Board.new
-knight = Knight.new([2, 1])
-board.adj_list[2] << 1 # placing the knight
-board.adj_list[2].pop(1) # step one of movement which is to delete old coordinates
-board.adj_list[3] << 3 # step two of movement which is to place knight on new coordinates
+knight = Knight.new
+# board.adj_list[2] << 1 # placing the knight
+# board.adj_list[2].pop(1) # step one of movement which is to delete old coordinates
+# board.adj_list[3] << 3 # step two of movement which is to place knight on new coordinates
+board.add_piece(knight.current_coordinate)
+p board.adj_list
+board.add_piece([4,2])
+board.remove_piece([4,2])
+board.remove_piece([2,1])
 p board.adj_list
